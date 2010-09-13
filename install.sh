bce853ec5095f6ee49369b855ef70b7b8a1df24e
Damien Plenard <damien@plenard.me>
2010-09-13 14:03:22 +0200

    chmod on install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,9 @@
 ## Login   <damienplenard@epitech.net>
 ## 
 ## Started on  Mon Sep 13 13:22:42 2010 Damien Plenard
-## Last update Mon Sep 13 13:30:00 2010 Damien Plenard
+## Last update Mon Sep 13 14:02:30 2010 Damien Plenard
 ##
 
-function main()
-{
-    echo "Config will be installed";
-    cp -fR .* ~/;
-    echo "Let's go";
-}
-
-main();
+echo "Config will be installed";
+exec cp -fR .* ~/
+echo "Let's go";
