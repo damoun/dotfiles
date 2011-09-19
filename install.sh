7166ddbcc269b1bb83f4b62b5a7255dbddbdb315
Damien Plenard <damien@plenard.me>
2011-09-19 20:38:10 +0200

    init function install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
-#! /bin/sh
+#!/bin/bash
 
+NAME="PersonalConfig"
 
-function init_install_path
+function init_install_path 
 {
-    INSTALL_PATH=$HOME
-    while [ "$INSTALL_PATH" == "" ]
+    INSTALL_PATH=${1:-"${HOME}"}
+    while [[ "$INSTALL_PATH" == "" || ! -d "$INSTALL_PATH" ]]
     do
-	echo "Home path can't be found !"
-	echo -n "Please enter this (or C-c): "
-	read $INSTALL_PATH
+	echo "Home path $INSTALL_PATH can't be found !"
+	echo -n "Please enter a valid path (or C-c to cancel): "
+	read INSTALL_PATH
     done
+    echo "$NAME will be installed in $INSTALL_PATH."
+    echo -n "Press 'y' to continue, 'e' to edit or 'c' to cancel: "
+    read CONFIRM
+    if [[ "$CONFIRM" == "y" || "$CONFIRM" == "Y" ]]
+    then
+    	exit 0
+    elif [[ "$CONFIRM" == "e" || "$CONFIRM" == "E" ]]
+    then
+    	read INSTALL_PATH
+    	init_install_path $INSTALL_PATH
+    fi
 }
 
-init_install_path
+init_install_path $1
