6b1a310420657cf538bed699b691705313a40fe8
Damien Plenard <damien@plenard.me>
2011-09-20 05:04:09 +0200

    refactor & add git install (thanks arcanin)

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Many of this code is a fork of MiniDump by arcanin
+# on https://github.com/arcanin/MiniDump
 
 NAME="PersonalConfig"
 
-function init_install_path 
+function init_install_path
 {
     INSTALL_PATH=${1:-"${HOME}"}
-    while [[ "$INSTALL_PATH" == "" || ! -d "$INSTALL_PATH" ]]
+    while [[ "$INSTALL_PATH" = "" || ! -d "$INSTALL_PATH" ]]
     do
 	echo "Home path $INSTALL_PATH can't be found !"
 	echo -n "Please enter a valid path (or C-c to cancel): "
@@ -14,14 +16,49 @@ function init_install_path
     echo "$NAME will be installed in $INSTALL_PATH."
     echo -n "Press 'y' to continue, 'e' to edit or 'c' to cancel: "
     read CONFIRM
-    if [[ "$CONFIRM" == "y" || "$CONFIRM" == "Y" ]]
+    if [[ "$CONFIRM" = "c" || "$CONFIRM" = "C" ]]
     then
     	exit 0
-    elif [[ "$CONFIRM" == "e" || "$CONFIRM" == "E" ]]
+    elif [[ "$CONFIRM" = "e" || "$CONFIRM" = "E" ]]
     then
     	read INSTALL_PATH
     	init_install_path $INSTALL_PATH
     fi
 }
 
+function install_package
+{
+    if ( hash "apt-get" >& /dev/null )
+    then
+	sudo apt-get -qq install "$1"
+    elif ( hash "yum" >& /dev/null )
+    then
+	sudo yum -q -y install "$1"
+    elif ( hash "pkg_add" >& /dev/null )
+    then
+	sudo pkg_add -r "$1" >& /dev/null
+    fi
+}
+
+function request_user_input
+{
+    local input;
+    read -p "$1: " input < /dev/tty;
+    echo $input
+}
+
+function exec_directory
+{
+    for dir in *
+    do
+	if [[ -d $dir && -x "$dir/install.sh" ]]
+	then
+	    echo "=== Install $dir ==="
+	    source "$dir/install.sh"
+	    echo "=== $dir installed ==="
+	fi
+    done
+}
+
 init_install_path $1
+exec_directory
