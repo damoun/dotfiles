d660cbb6649e2104c0ffc3f2f04099e16a7b2325
Damien Plenard <damien@plenard.me>
2012-02-05 17:59:26 +0100

    add ls++

---
+++ ls++/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cpan Term::ExtendedColor
+git clone git://github.com/trapd00r/ls--.git
+cd ls--
+perl Makefile.PL
+make && sudo make install
+cd ../
+cp ls++.conf $INSTALL_PATH/.ls++.conf
