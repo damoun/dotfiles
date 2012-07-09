d4222474b71f1a812469eaed6862f0828f4ec67a
Damien Plenard <damien@plenard.me>
2012-07-09 12:30:23 +0200

    added php mode

--- emacs/emacs.d/mode.el
+++ emacs/emacs.d/mode.el
@@ -5,3 +5,6 @@
 ;; (setq load-path (append (list (expand-file-name "~/.emacs.d/mode")) load-path))
 ;; (autoload 'js2-mode "js2-mode" nil t)
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; PHP
+(load "~/.emacs.d/mode/php-mode.el")
