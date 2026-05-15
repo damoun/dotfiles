2f9b958c93bf20a19a63a09ad8f80055949b6554
Damien Plénard <damien@plenard.me>
2026-05-16 01:11:42 +0200

    refactor: simplify host configurations by moving shared settings to common.nix

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -1,26 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
-    ../../modules/home-manager/git.nix
-    ../../modules/home-manager/zsh.nix
-    ../../modules/home-manager/tmux.nix
-    ../../modules/home-manager/gpg.nix
+    ../../modules/home-manager/common.nix
   ];
 
-  # home.username is set by nix-darwin if using the module usually,
-  # but it's good to be explicit or let it be passed.
   home.username = "damoun";
   home.homeDirectory = "/Users/damoun";
-
-  home.packages = with pkgs; [
-    neovim
-    fzf
-    jq
-    htop
-    tldr
-    gh
-  ];
-
   home.stateVersion = "24.05";
 }
--- hosts/ubuntu/home.nix
+++ hosts/ubuntu/home.nix
@@ -1,35 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
-    ../../modules/home-manager/git.nix
-    ../../modules/home-manager/zsh.nix
-    ../../modules/home-manager/tmux.nix
-    ../../modules/home-manager/gpg.nix
+    ../../modules/home-manager/common.nix
   ];
 
   home.username = "damoun";
   home.homeDirectory = "/home/damoun";
-
-  home.packages = with pkgs; [
-    neovim
-    fzf
-    jq
-    htop
-    tldr
-    gh
-  ];
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "24.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
