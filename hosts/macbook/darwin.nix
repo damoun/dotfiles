eafc5b1bb5458eb46b9fe5977632b1abdd8b823e
Damien Plénard <damien@plenard.me>
2026-05-16 00:30:50 +0200

    feat: add ubuntu and macbook host configurations and flake.nix

---
+++ hosts/macbook/darwin.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    # We will use home-manager as a module within nix-darwin
+  ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [
+    pkgs.vim
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+
+  # Set Git commit hash for darwin-rebuild changelog.
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # MacOS settings
+  system.defaults = {
+    dock.autohide = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.KeyRepeat = 2;
+  };
+}
---
+++ hosts/macbook/home.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/zsh.nix
+    ../../modules/home-manager/tmux.nix
+  ];
+
+  # home.username is set by nix-darwin if using the module usually,
+  # but it's good to be explicit or let it be passed.
+  home.username = "damoun";
+  home.homeDirectory = "/Users/damoun";
+
+  home.packages = with pkgs; [
+    neovim
+    fzf
+    jq
+    htop
+    tldr
+  ];
+
+  home.stateVersion = "24.05";
+}
