2e61230a74473f359f25fc33cbfa16a32d89911c
Damien Plénard <damien@plenard.me>
2026-05-16 17:49:32 +0200

    fix(ghostty): wrap with GSK_RENDERER=gl to resolve OpenGL issues

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -1,8 +1,20 @@
 { pkgs, ... }:
 
+let
+  # This is the version that the user said worked for the UI.
+  ghostty-wrapped = pkgs.symlinkJoin {
+    name = "ghostty";
+    paths = [ pkgs.ghostty ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/ghostty \
+        --set GSK_RENDERER gl
+    '';
+  };
+in
 {
   home.packages = [
-    pkgs.ghostty
+    ghostty-wrapped
   ];
 
   home.file.".config/ghostty/config".text = ''
